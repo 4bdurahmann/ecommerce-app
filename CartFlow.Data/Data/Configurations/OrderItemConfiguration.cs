53ac8811780e75593f7505fd315ea8a76036bd2f
Abdul-Rahman Gamal <abdulrahmangamal4u@gmail.com>
2026-07-23 00:22:20 +0300

    fix: handle buy-now flow in checkout POST handler

--- CartFlow.Data/Data/Configurations/OrderItemConfiguration.cs
+++ CartFlow.Data/Data/Configurations/OrderItemConfiguration.cs
@@ -2,12 +2,9 @@ using CartFlow.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace CartFlow.Data.Data.Configurations
-{
-    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
-    {
-        public void Configure(EntityTypeBuilder<OrderItem> builder)
-        {
+namespace CartFlow.Data.Data.Configurations {
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem> {
+        public void Configure(EntityTypeBuilder<OrderItem> builder) {
             builder.HasKey(oi => oi.Id);
 
             builder.HasOne(oi => oi.Order)
